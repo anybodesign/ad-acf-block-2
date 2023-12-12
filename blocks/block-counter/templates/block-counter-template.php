fc47bf18054e794e80121c3e2abb12e9362a7ef5
Thomas Villain <atelier@anybodesign.com>
2023-12-12 09:24:34 +0100

    Counter block sign

--- blocks/block-counter/templates/block-counter-template.php
+++ blocks/block-counter/templates/block-counter-template.php
@@ -28,14 +28,17 @@
 						<?php while( have_rows('counters') ): the_row(); 
 								
 							$num = get_sub_field('number');
+							$sign = get_sub_field('sign');
 							$inc = get_sub_field('increment');
 							$legend = get_sub_field('legend');
 						?>
 						
 						<div class="counter-item">
 							<?php if ( $num ) { ?>
-							<span class="counter-item--number title-data numscroller" data-min="1" data-max="<?php echo $num; ?>" data-delay="5" data-increment="<?php echo $inc; ?>">
-								<?php echo $num; ?>
+							<span class="counter-item--number">
+								<span class="title-data numscroller" data-min="1" data-max="<?php echo $num; ?>" data-delay="5" data-increment="<?php echo $inc; ?>">
+									<?php echo $num; ?>
+								</span><?php if ($sign) { echo $sign; } ?>
 							</span>
 							<?php } ?>
 							
